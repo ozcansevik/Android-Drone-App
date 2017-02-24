64289873f6ef260a7d145815e696dad0b4ce11f1
ozcansevik <sevik.ozcan@gmail.com>
2017-02-25 00:48:47 +0100

    Premiere version

---
+++ FPVDemo/app/src/main/java/com/dji/FPVDemo/Dialog_waypointsetting.java
@@ -0,0 +1,13 @@
+package com.dji.FPVDemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Dialog_waypointsetting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_waypointsetting);
+    }
+}
